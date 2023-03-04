dc410a63f6a4891d3da99ba23341687b257591fd
Mariya Kolbintseva <kolbintsevam@mail.ru>
2023-03-04 20:44:49 +0300

    Add application.package

---
+++ application/db/people.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from emoji import emojize
+
+def get_employees():
+    print("People")
+    print(datetime.now())
+    print(emojize('Python is :fire:'))
---
+++ application/salary.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from emoji import emojize
+
+def calculate_salary():
+    print("Salary")
+    print(datetime.now())
+    print(emojize('Python is :thumbs_up:'))
