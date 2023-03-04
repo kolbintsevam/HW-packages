790ad1f6ef9ecc71443cef4d6ca89271a3621b5a
Mariya Kolbintseva <kolbintsevam@mail.ru>
2023-03-04 20:47:29 +0300

    Add main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from application.db.people import get_employees
+from application.salary import calculate_salary
+
+
+
+if __name__ == '__main__':
+    get_employees()
+    print()
+    calculate_salary()
